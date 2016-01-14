5cb33e2aef4d6f44a3b0fea4744ea86bc2c07a89
Erik Kramli <nixon91erik@gmail.com>
2016-01-14 23:07:08 +0100

    Created cache fallback request listener. Initial solution, needs sophisticated onSuccess and onFailure callbacks.

--- SeriesCruncher/app/src/main/java/com/balazs_csernai/seriescruncher/rest/RestServiceImpl.java
+++ SeriesCruncher/app/src/main/java/com/balazs_csernai/seriescruncher/rest/RestServiceImpl.java
@@ -2,15 +2,15 @@ package com.balazs_csernai.seriescruncher.rest;
 
 import android.app.Activity;
 
-import com.balazs_csernai.seriescruncher.details.model.ShowDetailsEntity;
 import com.balazs_csernai.seriescruncher.details.model.ShowDetailsModel;
 import com.balazs_csernai.seriescruncher.details.request.ShowDetailsRequest;
-import com.balazs_csernai.seriescruncher.rest.api.epguides.EPGuideApi;
-import com.balazs_csernai.seriescruncher.rest.api.omdb.OmdbApi;
 import com.balazs_csernai.seriescruncher.rest.api.epguides.model.ShowShortList;
-import com.balazs_csernai.seriescruncher.rest.request.AllShowRequest;
 import com.balazs_csernai.seriescruncher.rest.request.RequestFactory;
 import com.octo.android.robospice.SpiceManager;
+import com.octo.android.robospice.exception.NetworkException;
+import com.octo.android.robospice.exception.NoNetworkException;
+import com.octo.android.robospice.exception.RequestCancelledException;
+import com.octo.android.robospice.persistence.DurationInMillis;
 import com.octo.android.robospice.persistence.exception.SpiceException;
 import com.octo.android.robospice.request.listener.RequestListener;
 
@@ -61,16 +61,45 @@ public class RestServiceImpl implements RestService {
 
     @Override
     public void loadDetails(String showName, String imdbId, final Callback<ShowDetailsModel> callback) {
-        spiceManager.execute(requestFactory.createShowDetailsRequest(showName, imdbId), new RequestListener<ShowDetailsEntity>() {
-            @Override
-            public void onRequestFailure(SpiceException spiceException) {
+        ShowDetailsRequest request = requestFactory.createShowDetailsRequest(showName, imdbId);
+        spiceManager.execute(request, showName, DurationInMillis.ALWAYS_EXPIRED, new CacheFallbackRequestListener<>(request.getResultType(), showName, callback));
+    }
+
+    final class CacheFallbackRequestListener<MODEL, JSON extends MODEL> implements RequestListener<JSON> {
+
+        private final Class<JSON> resultType;
+        private final String cacheKey;
+        private final Callback<MODEL> callback;
+
+        CacheFallbackRequestListener(Class<JSON> resultType, String cacheKey, Callback<MODEL> callback) {
+            this.resultType = resultType;
+            this.cacheKey = cacheKey;
+            this.callback = callback;
+        }
+
+        @Override
+        public void onRequestSuccess(JSON json) {
+            if (json == null) {
+                callback.onFailure();
+            } else {
+                callback.onSuccess(json);
+            }
+        }
+
+        @Override
+        public void onRequestFailure(SpiceException spiceException) {
+            if (isNetworkException(spiceException)) {
+                spiceManager.getFromCache(resultType, cacheKey, DurationInMillis.ALWAYS_RETURNED, this);
+
+            } else {
                 callback.onFailure();
             }
+        }
 
-            @Override
-            public void onRequestSuccess(ShowDetailsEntity showDetailsEntity) {
-                callback.onSuccess(showDetailsEntity);
-            }
-        });
+        private boolean isNetworkException(SpiceException spiceException) {
+            return spiceException instanceof NetworkException
+                    || spiceException instanceof NoNetworkException
+                    || spiceException instanceof RequestCancelledException;
+        }
     }
 }
