07161eb7d730484cf516aca9366bcee1eee2373b
Erik <erik_markus_kramli@epam.com>
2016-02-02 12:26:52 +0100

    Fixed text color logic in ColorProvider

--- SeriesCruncher/app/src/main/java/com/balazs_csernai/seriescruncher/utils/color/ColorProviderImpl.java
+++ SeriesCruncher/app/src/main/java/com/balazs_csernai/seriescruncher/utils/color/ColorProviderImpl.java
@@ -2,9 +2,11 @@ package com.balazs_csernai.seriescruncher.utils.color;
 
 import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.support.annotation.ColorInt;
 import android.support.annotation.ColorRes;
 import android.support.v7.graphics.Palette;
+import android.support.v7.graphics.Palette.Swatch;
 
 import com.balazs_csernai.seriescruncher.R;
 
@@ -15,8 +17,10 @@ import javax.inject.Inject;
  */
 public class ColorProviderImpl implements ColorProvider {
 
+    private static final int BRIGHTNESS_THRESHOLD = 200;
+
     private final Resources resources;
-    private Palette.Swatch swatch;
+    private Swatch swatch;
 
     @Inject
     public ColorProviderImpl(Resources resources) {
@@ -26,14 +30,41 @@ public class ColorProviderImpl implements ColorProvider {
     @Override
     public void generateColorPalette(Bitmap bitmap) {
         if (bitmap != null) {
-            swatch = Palette.from(bitmap).generate().getSwatches().get(1);
+            Palette palette = Palette.from(bitmap).generate();
+            swatch = getSwatch(palette.getDarkVibrantSwatch(),
+                    palette.getMutedSwatch(),
+                    palette.getVibrantSwatch(),
+                    palette.getSwatches().get(0));
         }
+    }
+
+
+    private Swatch getSwatch(Swatch... swatches) {
+        Swatch desiredSwatch = null;
+        for (Swatch swatch : swatches) {
+            if (swatch != null) {
+                desiredSwatch = swatch;
+                break;
+            }
+        }
+        return desiredSwatch;
     }
 
     @ColorInt
     @Override
     public int getTextColor() {
-        return swatch == null ? getColor(R.color.white) : swatch.getTitleTextColor();
+        int color = getBackgroundColor();
+        int[] rgb = {Color.red(color), Color.green(color), Color.blue(color)};
+        int brightness = (int)Math.sqrt(
+                rgb[0] * rgb[0] * .241 +
+                rgb[1] * rgb[1] * .691 +
+                rgb[2] * rgb[2] * .068);
+
+        if (brightness >= BRIGHTNESS_THRESHOLD) {
+            return getColor(R.color.black);
+        } else {
+            return getColor(R.color.white);
+        }
     }
 
     @ColorInt
