7ddbdbf93fac2ebbd71197d4bf3c80f2dd84af5c
Erik <erik_markus_kramli@epam.com>
2016-02-09 13:39:03 +0100

    Implemented next or last episode logic in series details presenter.

--- SeriesCruncher/app/src/main/java/com/balazs_csernai/seriescruncher/seriesdetails/presenter/SeriesDetailsPresenterImpl.java
+++ SeriesCruncher/app/src/main/java/com/balazs_csernai/seriescruncher/seriesdetails/presenter/SeriesDetailsPresenterImpl.java
@@ -1,15 +1,22 @@
 package com.balazs_csernai.seriescruncher.seriesdetails.presenter;
 
+import android.util.Log;
+
 import com.balazs_csernai.seriescruncher.preferences.PreferenceHandler;
 import com.balazs_csernai.seriescruncher.preferences.user.UserPreferencesModel;
 import com.balazs_csernai.seriescruncher.rest.SeriesLoader;
 import com.balazs_csernai.seriescruncher.rest.loader.Loader.Callback;
 import com.balazs_csernai.seriescruncher.seriesdetails.model.SeriesDetailsModel;
 import com.balazs_csernai.seriescruncher.seriesdetails.model.episode.EpisodeListModel;
+import com.balazs_csernai.seriescruncher.seriesdetails.model.episode.EpisodeModel;
 import com.balazs_csernai.seriescruncher.seriesdetails.model.poster.PosterModel;
 import com.balazs_csernai.seriescruncher.seriesdetails.ui.SeriesDetailsScreen;
+import com.balazs_csernai.seriescruncher.utils.common.DateUtils;
 import com.balazs_csernai.seriescruncher.utils.converter.EpisodeList;
 import com.balazs_csernai.seriescruncher.utils.converter.ModelConverter;
+
+import java.util.Date;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -74,12 +81,48 @@ public class SeriesDetailsPresenterImpl implements SeriesDetailsPresenter, Serie
             screen.setPoster(result.getPoster());
             screen.setBackground(result.getPosterBackground());
             screen.displaySeriesDetails((EpisodeListModel) converter.convert(detailsModel));
+            displayNextOrLastEpisode();
         }
 
         @Override
         public void onFailure() {
         }
     };
+
+    private void displayNextOrLastEpisode() {
+        int lastAiredEpisodeIndex = findLastAiredEpisodeIndex(detailsModel.getEpisodes());
+        EpisodeModel nextEpisode = getEpisode(lastAiredEpisodeIndex - 1);
+        if (nextEpisode != null) {
+            screen.displayNextEpisode(nextEpisode);
+
+        } else {
+            EpisodeModel lastEpisode = getEpisode(lastAiredEpisodeIndex);
+            screen.displayLastEpisode(lastEpisode);
+        }
+    }
+
+    private EpisodeModel getEpisode(int episodeIndex) {
+        try {
+            return detailsModel.getEpisodes().get(episodeIndex);
+        } catch (IndexOutOfBoundsException e) {
+            return null;
+        }
+    }
+
+    private int findLastAiredEpisodeIndex(List<EpisodeModel> episodes) {
+        int index = -1;
+        for (int i = 0, n = episodes.size(); i < n && index == -1; i++) {
+            if (isLastEpisode(episodes.get(i))) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+    private boolean isLastEpisode(EpisodeModel episode) {
+        Date airDate = episode.getAirDate();
+        return airDate != null && DateUtils.getTodayDate().after(airDate);
+    }
 
     @Override
     public void onFavorFabClicked() {
