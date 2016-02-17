a1baae70ba6fff75516a993c28983d3afeb2da2e
Balazs_Csernai <Balazs_Csernai@epam.com>
2016-02-17 11:57:45 +0100

    Since support alert dialog styling is not working as expected on API 18, a custom list (adapter, listener) implementation is needed for alert dialog answer options.

--- SeriesCruncher/app/src/main/java/com/balazs_csernai/seriescruncher/utils/dialog/NetworkErrorDialogFragment.java
+++ SeriesCruncher/app/src/main/java/com/balazs_csernai/seriescruncher/utils/dialog/NetworkErrorDialogFragment.java
@@ -1,17 +1,25 @@
 package com.balazs_csernai.seriescruncher.utils.dialog;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import com.balazs_csernai.seriescruncher.R;
 
 import java.util.Random;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * Created by Balazs_Csernai on 2016.02.08..
@@ -51,27 +59,92 @@ public class NetworkErrorDialogFragment extends DialogFragment {
         networkErrorRetryResponses = resources.getStringArray(R.array.network_error_retry_responses);
         networkErrorCancelResponses = resources.getStringArray(R.array.network_error_cancel_responses);
 
+        String[] items = new String[]{
+                getRandomText(networkErrorRetryResponses),
+                getRandomText(networkErrorCancelResponses)
+        };
+
         return new AlertDialog.Builder(getActivity(), R.style.AlertDialogStyle)
                 .setTitle(getRandomText(networkErrorTitles))
-                .setItems(new String[]{getRandomText(networkErrorRetryResponses), getRandomText(networkErrorCancelResponses)}, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                        switch (which) {
-                            case 0:
-                                callback.onNetworkErrorRetry();
-                                break;
-                            case 1:
-                            default:
-                                callback.onNetworkErrorCancel();
-                        }
-                    }
-                })
+                .setAdapter(new CustomAdapter(items), new CustomListener())
                 .setCancelable(false)
                 .create();
     }
 
     public String getRandomText(String[] texts) {
         return texts[random.nextInt(texts.length)];
+    }
+
+    class CustomAdapter extends BaseAdapter {
+        private final String[] items;
+
+        public CustomAdapter(String[] items) {
+            this.items = items;
+        }
+
+        @Override
+        public int getCount() {
+            return items.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            ViewHolder holder;
+
+            if (view == null) {
+                LayoutInflater.from(parent.getContext()).inflate(R.layout.alert_dialog_list_item, parent, false);
+                holder = new ViewHolder(view);
+                view.setTag(holder);
+            } else {
+                holder = (ViewHolder) view.getTag();
+            }
+
+            populate(holder, position);
+
+            return view;
+        }
+
+        private void populate(ViewHolder holder, int position) {
+            holder.setText(items[position]);
+        }
+
+        class ViewHolder {
+            @InjectView(R.id.mainText)
+            TextView mainText;
+
+            public ViewHolder(View view) {
+                ButterKnife.inject(this, view);
+            }
+
+            public void setText(String text) {
+                mainText.setText(text);
+            }
+        }
+    }
+
+    class CustomListener implements DialogInterface.OnClickListener {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            dialog.dismiss();
+            switch (which) {
+                case 0:
+                    callback.onNetworkErrorRetry();
+                    break;
+                case 1:
+                default:
+                    callback.onNetworkErrorCancel();
+            }
+        }
     }
 }
